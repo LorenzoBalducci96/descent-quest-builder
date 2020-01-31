00bf7ef896965557803649ff045235b70ea60d8c
Lorenzo Balducci <lorenzo.balducci96@outlook.it>
2020-01-31 19:29:28 +0100

    add shadow of nerekhall expansion tiles

--- js/loadTiles.js
+++ js/loadTiles.js
@@ -1,17 +1,42 @@
 var activeSet = "baseSetOutside"
-var activeArea = "outside";
+var activeArea = "Outside";
+var activeSetName = "baseSet";
 
-function LoadShadowOfNerekhall() {
+
+
+function LoadShadowOfNerekhallOutside() {
     backupToInvisibleDiv();
-    document.getElementById("sidenav").innerHTML = document.getElementById("shadowOfNerekhallElements").innerHTML;
-    activeSet = "shadowOfNerekhallElements";
+    document.getElementById("sidenav").innerHTML = document.getElementById("shadowOfNerekhallOutside").innerHTML;
+    document.getElementById('insideButton').onclick = LoadShadowOfNerekhallInside
+    document.getElementById('miscellaneousButton').style.display = "none"
+    activeSet = "shadowOfNerekhallOutside";
     loadAndArrangeTiles();
 }
 
-function LoadLabyrinthOfRuin() {
+function LoadShadowOfNerekhallInside() {
     backupToInvisibleDiv();
-    document.getElementById("sidenav").innerHTML = document.getElementById("labyrinthOfRuinElements").innerHTML;
-    activeSet = "labyrinthOfRuinElements";
+    document.getElementById("sidenav").innerHTML = document.getElementById("shadowOfNerekhallInside").innerHTML;
+    document.getElementById('insideButton').onclick = LoadShadowOfNerekhallOutside
+    document.getElementById('miscellaneousButton').style.display = "none"
+    activeSet = "shadowOfNerekhallInside";
+    loadAndArrangeTiles();
+}
+
+function LoadLabyrinthOfRuinOutside() {
+    backupToInvisibleDiv();
+    document.getElementById("sidenav").innerHTML = document.getElementById("labyrinthOfRuinOutside").innerHTML;
+    document.getElementById('insideButton').onclick = LoadLabyrinthOfRuinInside
+    document.getElementById('miscellaneousButton').style.display = "none"
+    activeSet = "labyrinthOfRuinInside";
+    loadAndArrangeTiles();
+}
+
+function LoadLabyrinthOfRuinInside() {
+    backupToInvisibleDiv();
+    document.getElementById("sidenav").innerHTML = document.getElementById("labyrinthOfRuinInside").innerHTML;
+    document.getElementById('insideButton').onclick = LoadLabyrinthOfRuinOutside
+    document.getElementById('miscellaneousButton').style.display = "none"
+    activeSet = "labyrinthOfRuinInside";
     loadAndArrangeTiles();
 }
 
@@ -22,40 +47,43 @@ function backupToInvisibleDiv() {
         document.getElementById("baseSetInside").innerHTML = document.getElementById("sidenav").innerHTML;
     } else if (activeSet == "baseSetMiscellaneous") {
         document.getElementById("baseSetMiscellaneous").innerHTML = document.getElementById("sidenav").innerHTML;
-    } else if (activeSet == "shadowOfNerekhallElements") {
-        document.getElementById("shadowOfNerekhallElements").innerHTML = document.getElementById("sidenav").innerHTML;
+    } else if (activeSet == "LoadShadowOfNerekhallOutside") {
+        document.getElementById("LoadShadowOfNerekhallOutside").innerHTML = document.getElementById("sidenav").innerHTML;
+    } else if (activeSet == "LoadShadowOfNerekhallInside") {
+        document.getElementById("LoadShadowOfNerekhallInside").innerHTML = document.getElementById("sidenav").innerHTML;
     } else if (activeSet == "labyrinthOfRuinElements") {
         document.getElementById("labyrinthOfRuinElements").innerHTML = document.getElementById("sidenav").innerHTML;
     }
 }
 
-function loadMiscellaneous() {
+function loadBaseSetMiscellaneous() {
     backupToInvisibleDiv();
-    if (activeSet == "baseSetOutside" || activeSet == "baseSetInside") { //we will put check for expansions
+    //if (activeSet == "baseSetOutside" || activeSet == "baseSetInside") { //we will put check for expansions
         document.getElementById("sidenav").innerHTML = document.getElementById("baseSetMiscellaneous").innerHTML;
+        document.getElementById('miscellaneousButton').style.display = ""
         activeSet = "baseSetMiscellaneous"
         loadAndArrangeTiles();
-    }
+    //}
 }
 
 function loadBaseSetInside() {
     backupToInvisibleDiv();
-    if (activeSet == "baseSetOutside" || activeSet == "baseSetMiscellaneous") {
+    //if (activeSet == "baseSetOutside" || activeSet == "baseSetMiscellaneous") {
         document.getElementById("sidenav").innerHTML = document.getElementById("baseSetInside").innerHTML;
+        document.getElementById('insideButton').onclick = loadBaseSetOutside
+        document.getElementById('miscellaneousButton').style.display = ""
         activeSet = "baseSetInside";
         loadAndArrangeTiles();
-    }
+    //}
 }
 
 function loadBaseSetOutside() {
     backupToInvisibleDiv();
-    if (activeSet == "baseSetInside" || activeSet == "baseSetMiscellaneous") {
+    //if (activeSet == "baseSetInside" || activeSet == "baseSetMiscellaneous") {
         document.getElementById("sidenav").innerHTML = document.getElementById("baseSetOutside").innerHTML;
+        document.getElementById('insideButton').onclick = loadBaseSetInside
+        document.getElementById('miscellaneousButton').style.display = ""
         activeSet = "baseSetOutside";
         loadAndArrangeTiles();
-    }
-}
-
-function insideOutsideTrigger(){
-
+    //}
 }
