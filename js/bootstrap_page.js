e2915f8c9aefd4e83e454586ffc345764f59038b
Lorenzo Balducci <lorenzo.balducci3@studio.unibo.it>
2021-05-23 11:16:54 +0200

    Before this commit just the set of tiles "base set outside" was loaded. Now all the tiles are loaded in the bootstrap of the page the funcion loadAndArrangeTiles is called for all the tile's groups

--- js/bootstrap_page.js
+++ js/bootstrap_page.js
@@ -18,6 +18,22 @@ function bootstrap_page() {
     setTimeout(() => {
         //loadAndArrange decide the position of each tile in his bar
         this.loadAndArrangeTiles("baseSetOutside");
+        this.loadAndArrangeTiles("baseSetInside");
+        this.loadAndArrangeTiles("baseSetMiscellaneous");
+        this.loadAndArrangeTiles("shadowOfNerekhallOutside");
+        this.loadAndArrangeTiles("shadowOfNerekhallInside");
+        this.loadAndArrangeTiles("shadowOfNerekhallMiscellaneous");
+        this.loadAndArrangeTiles("labyrinthOfRuinOutside");
+        this.loadAndArrangeTiles("labyrinthOfRuinInside");
+        this.loadAndArrangeTiles("lairOfTheWyrmOutside");
+        this.loadAndArrangeTiles("lairOfTheWyrmInside");
+        this.loadAndArrangeTiles("trollfensOutside");
+        this.loadAndArrangeTiles("trollfensInside");
+        this.loadAndArrangeTiles("tokens");
+        this.loadAndArrangeTiles("monsters");
+        this.loadAndArrangeTiles("lieutenants");
+        this.loadAndArrangeTiles("miscellaneous");
+        this.loadAndArrangeTiles("heroes");
 
         window.addEventListener("resize", rearrangeAllTiles);//we want to rearrange all tiles on zoom change
         window.addEventListener("resize", trickForCorrectGoogleChromeResizeBug);
